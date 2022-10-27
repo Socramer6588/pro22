c38a568d017fb3b07447e8f779042f3f356b7d89
Marcos <marcos6588@gmail.com>
2022-10-27 17:07:40 +0200

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un número: ");
+        num = sc.next();
+        System.out.println("El número" + " introducido es: " + num);
+    }
+}
+
