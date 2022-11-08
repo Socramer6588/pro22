9f426dfd9d91ec148ffde4991138d04586cf7a82
Marcos Domínguez Ordóñez <a19marcosdo@ateis.local>
2022-11-08 18:25:24 +0100

    Clase

--- src/com/programacion/boletin1/ejercicio4/Main.java
+++ src/com/programacion/boletin1/ejercicio4/Main.java
@@ -2,10 +2,16 @@ package com.programacion.boletin1.ejercicio4;
 
 import java.util.Scanner;
 
-public class Main {
+import static java.lang.Math.pow;
+
+public class Main { //TODO recursive solution & N Digits
 
     static Scanner sc = new Scanner(System.in);
     static int number;
+    static int numberCopyForMethods;
+    static int auxArray;
+    static int[] arraySeparateNumber = new int[3];
+    static boolean armstrong;
 
     public static void entryInterface() {
         System.out.println(" ");
@@ -14,16 +20,36 @@ public class Main {
         number = sc.nextInt();
     }
 
+    public static void fillArray() {
+        arraySeparateNumber[auxArray] = numberCopyForMethods % 10;
+        numberCopyForMethods = numberCopyForMethods / 10;
+        auxArray++;
+        if (numberCopyForMethods > 0) {
+            fillArray();
+        }
+    }
+
+    public static void ordenateArray() {
+
+    }
+
     public static void calculateArmstrong() {
-        
+        armstrong = (pow(arraySeparateNumber[0], 3) + pow(arraySeparateNumber[1], 3) + pow(arraySeparateNumber[2], 3)) == number;
     }
 
     public static void showSolution() {
-
+        if (armstrong) {
+            System.out.println("Es un número Armstrong");
+        } else {
+            System.out.println("No es un número Armstrong");
+        }
     }
 
     public static void main(String[] args) {
         entryInterface();
+        numberCopyForMethods = number;
+        fillArray();
+        ordenateArray();
         calculateArmstrong();
         showSolution();
     }
