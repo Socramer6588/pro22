8ba86a713073b6d8b0711cfb27b2e198e03c763b
Marcos Domínguez Ordóñez <a19marcosdo@ATEIS.LOCAL>
2022-11-08 16:37:33 +0100

    Commit for Ángel

--- src/com/programacion/boletin1/ejercicio4/Main.java
+++ src/com/programacion/boletin1/ejercicio4/Main.java
@@ -1,7 +1,30 @@
 package com.programacion.boletin1.ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int number;
+
+    public static void entryInterface() {
+        System.out.println(" ");
+        System.out.println("Escribe un número y vamos a comprobar si es un número Armstrong: ");
+        System.out.println("Ej: 153 = 1^3 + 5^3 + 3^3");
+        number = sc.nextInt();
+    }
+
+    public static void calculateArmstrong() {
+        
+    }
+
+    public static void showSolution() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        entryInterface();
+        calculateArmstrong();
+        showSolution();
     }
 }
