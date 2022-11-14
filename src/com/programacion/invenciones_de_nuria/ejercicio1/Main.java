7b345da6aa5b2e1a76fef813ec836fc51c395012
Marcos Domínguez Ordóñez <a19marcosdo@ateis.local>
2022-11-14 17:19:09 +0100

    Update Ejercicio1

--- src/com/programacion/invenciones_de_nuria/ejercicio1/Main.java
+++ src/com/programacion/invenciones_de_nuria/ejercicio1/Main.java
@@ -2,15 +2,46 @@ package com.programacion.invenciones_de_nuria.ejercicio1;
 
 public class Main {
 
+    static boolean primo;
     static int[] arrayNum = new int[30];
+    static int[] arrayPrimo = new int[30];
 
-    public static void main(String[] args) {
+    public static void llenarArray() {
         for (int aux = 0; aux < arrayNum.length; aux++) {
             arrayNum[aux] = (int) (Math.random() * 100);
         }
+    }
 
-        for (int num : arrayNum) {
-            System.out.println(num);
+    public static void encontrarPrimo() {
+        for (int aux = 0; aux < arrayNum.length; aux++) {
+            int contador = 0;
+
+            for (int i = 1; i <= arrayNum[i]; i++) {
+                if (arrayNum[aux] % i == 0) {
+                    contador++;
+                }
+            }
+
+            primo = contador == 2;
+
+            if (primo) {
+                arrayPrimo[aux] = arrayNum[aux];
+            }
+
         }
+    }
+
+    public static void mostrarResultado() {
+        for (int num : arrayPrimo) {
+            if (num != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        llenarArray();
+        encontrarPrimo();
+        mostrarResultado();
     }
 }
