a2f0c4ac4c95a2f49bc281ad548596f853516bb7
Marcos <92857974+Socramer6588@users.noreply.github.com>
2022-11-17 21:45:01 +0100

    Create Ejercicio4

---
+++ src/com/programacion/boletin2/ejercicio4/Main.java
@@ -0,0 +1,34 @@
+package com.programacion.boletin2.ejercicio4;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int aux = 0; aux <= 3; aux++) {
+            int astrx = 2 * aux - 1;
+            int oblx = 3 - aux;
+            for (int i = 1; i <= oblx; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int i = 1; i <= astrx; i++) {
+                System.out.print(" ");
+            }
+            if (aux != 0) System.out.print("*");
+            System.out.println();
+            System.out.println();
+        }
+        for (int aux = 2; aux >= 0; aux--) {
+            int astrx = 2 * aux - 1;
+            int oblx = 3 - aux;
+            for (int i = 1; i <= oblx; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int i = 1; i <= astrx; i++) {
+                System.out.print(" ");
+            }
+            if (aux != 0) System.out.print("*");
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
