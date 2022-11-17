aad5b3d684ee9599c9744fcfd60bc48fffe86965
Marcos <92857974+Socramer6588@users.noreply.github.com>
2022-11-17 19:38:06 +0100

    Create Ejercicio2

---
+++ src/com/programacion/boletin2/ejercicio2/Main.java
@@ -0,0 +1,14 @@
+package com.programacion.boletin2.ejercicio2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int aux = 1; aux <= 5; aux++) {
+            for (int i = 1; i <= aux; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
