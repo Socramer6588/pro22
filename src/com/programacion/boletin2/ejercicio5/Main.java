e0a96a97878bfb30fa2d047e17beca1b37f2521f
Marcos <92857974+Socramer6588@users.noreply.github.com>
2022-11-18 14:54:03 +0100

    Update Boletin2

--- src/com/programacion/boletin2/ejercicio5/Main.java
+++ src/com/programacion/boletin2/ejercicio5/Main.java
@@ -2,7 +2,7 @@ package com.programacion.boletin2.ejercicio5;
 
 import static com.programacion.boletin1.ejercicio5.Main.*;
 
-public class Main {
+public class Main { //Same requirements that in com.programacion.boletin1.ejercicio5
     public static void main(String[] args) {
         entryInterface();
         separateBirthDate();
---
+++ src/com/programacion/boletin2/ejercicio8/Main.java
@@ -0,0 +1,8 @@
+package com.programacion.boletin2.ejercicio8;
+
+public class Main { // TODO Same requirements that in com.programacion.boletin1.ejercicio6
+
+    public static void main(String[] args) {
+
+    }
+}
