5721cc166024d010f11c01f886c4da9f812bb10a
Marcos <marcos6588@gmail.com>
2022-11-25 14:21:55 +0100

    Update Ejercicio1

---
+++ src/ev2/uml/ejercicio1/Equipo.java
@@ -0,0 +1,28 @@
+package ev2.uml.ejercicio1;
+
+public class Equipo {
+
+    private String nombre;
+    private double presupuesto;
+
+    public Equipo(String nombre, double presupuesto) {
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+}
---
+++ src/ev2/uml/ejercicio1/Jugador.java
@@ -0,0 +1,58 @@
+package ev2.uml.ejercicio1;
+
+public class Jugador {
+
+    private String DNI;
+    private String nombre;
+    private String apellidos;
+    private int anhoNacimiento;
+    private Equipo equipo;
+
+    public Jugador(String DNI, String nombre, String apellidos, int anhoNacimiento, Equipo equipo) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.anhoNacimiento = anhoNacimiento;
+        this.equipo = equipo;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getAnhoNacimiento() {
+        return anhoNacimiento;
+    }
+
+    public void setAnhoNacimiento(int anhoNacimiento) {
+        this.anhoNacimiento = anhoNacimiento;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+}
