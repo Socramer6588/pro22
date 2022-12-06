2819a98c896b878c00f34d09e21affa595eaac08
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-06 22:40:30 +0100

    Update Boletin2

--- src/ev1/boletin2/ejercicio5/Main.java
+++ src/ev1/boletin2/ejercicio5/Main.java
@@ -2,7 +2,7 @@ package ev1.boletin2.ejercicio5;
 
 import static ev1.boletin1.ejercicio5.Main.*;
 
-public class Main { //Same requirements that in com.programacion.boletin1.ejercicio5
+public class Main {
     public static void main(String[] args) {
         entryInterface();
         separateBirthDate();
--- src/ev1/boletin2/ejercicio8/Main.java
+++ src/ev1/boletin2/ejercicio8/Main.java
@@ -2,7 +2,7 @@ package ev1.boletin2.ejercicio8;
 
 import static ev1.boletin1.ejercicio6.Main.*;
 
-public class Main { //Same requirements that in com.programacion.boletin1.ejercicio6
+public class Main {
 
     public static void main(String[] args) {
         entryInterface();
