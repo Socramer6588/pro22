fc674fa645102b102fa9f6f1608c3930a8a0c120
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2023-01-10 18:19:52 +0100

    Create Conversor

---
+++ src/ev2/uml/conversor/Converter.java
@@ -0,0 +1,22 @@
+package ev2.uml.conversor;
+
+public class Converter {
+    private final Integer number;
+
+    public Converter(Integer number) {
+        this.number = number;
+    }
+
+    public String getNumber(char format) {
+        if (format == 66 || format == 72 || format == 79) {
+            if (format == 'B') {
+                return Integer.toBinaryString(number);
+            } else if (format == 'H') {
+                return Integer.toHexString(number);
+            } else if (format == 'O') {
+                return Integer.toOctalString(number);
+            }
+        }
+        return "manco";
+    }
+}
---
+++ src/ev2/uml/conversor/Main.java
@@ -0,0 +1,14 @@
+package ev2.uml.conversor;
+
+public class Main {
+    public static void main(String[] args) {
+        Converter converter = new Converter(20);
+        System.out.println(converter.getNumber('B'));
+        System.out.println(converter.getNumber('H'));
+        System.out.println(converter.getNumber('O'));
+        System.out.println(converter.getNumber((char) 66));
+        System.out.println(converter.getNumber((char) 72));
+        System.out.println(converter.getNumber((char) 79));
+        System.out.println(converter.getNumber((char) 100));
+    }
+}
